ede7546aeff66fd574158719715310fbf4e1a486
SkyDoge69 <luc2002penta@gmail.com>
2020-07-06 13:47:09 +0300

    Typo fixed

--- database_conection/test.py
+++ database_conection/test.py
@@ -3,6 +3,6 @@ import sqlalchemy as sal
 from sqlalchemy import create_engine
 
 #Connecting to database and printing names of tables
-engine = sal.create_engine("mssql+pyodbc://SA:password@localhost/CentaurEventTestCopy?driver=ODBC+Driver+17+for+SQL+Server")
+engine = sal.create_engine("mssql+pyodbc://sa:Strong_Password@localhost/CentaurEventTestCopy?driver=SQL+Server")
 conn = engine.connect()
 print(engine.table_names())
