da2e1a4caaa531f733dfb5de8e659640f0a81e3a
bobig6 <b.i.ivanov1@gmail.com>
2020-07-07 13:13:15 +0300

    Adding class for Event

--- database_connection/test.py
+++ database_connection/test.py
@@ -1,11 +1,28 @@
 from database import DBConnection
+from event import Event
 
 
+result = DBConnection.engine.execute(
+    '''select [Event ID], [Event Names].Name as EventName, [Field Time], [Card Number], [Record Names].Name from Events
+    left join [Event Names] on Events.[Event Type] = [Event Names].[Event Type]
+    left join [Card Holder Names] on Events.[Card Holder ID] = [Card Holder Names].[Card Holder ID]
+    left join [Record Names] on Events.[Record Name ID] = [Record Names].[Record Name ID]
+    where [Event ID] = 69
 
-result = DBConnection.engine.execute("select * from Events where [Event ID] = 1")
+    
+    ''')
 for row in result:
-    print("Time:", row['Field Time'])
+    eventID = row["Event ID"]
+    eventType = row["EventName"]
+    eventTime = row["Field Time"]
+    cardNumber = row["Card Number"]
+    recordName = row["Name"]
+    print(eventID)
+    print(eventType)
+    print(eventTime)
+    print(cardNumber)
+    print(recordName)
 
 
-print(DBConnection.engine)
+
 
