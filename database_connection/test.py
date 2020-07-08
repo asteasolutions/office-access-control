454d354f47a316036c77a4760f57fbb64482ecdd
bobig6 <b.i.ivanov1@gmail.com>
2020-07-08 14:20:24 +0300

    Replacing row sql statements with orm for sql injection protection

--- database_connection/test.py
+++ database_connection/test.py
@@ -1,13 +1,28 @@
 from database import DBConnection
 from event import Event
 from sqlalchemy.sql import select
+from db_modules import Events, Event_Names, Record_Names, Card_Holder
 
-s = select(['Events.[Event ID]'])
-result = DBConnection.conn.execute(s)
+# find_it = select([Events.id, Event_Names.name, Events.field_time, Card_Holder.card_num, Record_Names.name]).select_from(Events.__table__
+# .join(Event_Names,Event_Names.event_type.like(Events.event_type))
+# .join(Card_Holder,Card_Holder.id.like(Events.card_holder_id))
+# .join(Record_Names,Record_Names.record_name_id.like(Events.record_name_id))).where(Events.id == 15)
+# print(str(find_it))
 
 
-# for i in Event.get_by_date('2019-11-11'):
-#     i.print_event()
+# rs = DBConnection.engine.execute(find_it)
+
+# events = []
+
+# for row in rs:
+#     events.append(Event(*row))
+
+# events[0].print_event()
+
+
+
+for i in Event.get_by_date('2019-11-11'):
+    i.print_event()
 
 
 
