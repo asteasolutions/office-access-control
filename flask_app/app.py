ab9229de0dbddadc00cfafea7a4fe95e03f64a31
SkyDoge69 <luc2002penta@gmail.com>
2020-07-09 12:07:36 +0300

    Info formating, sort by hour, swiper removed

--- flask_app/app.py
+++ flask_app/app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 # app.config['SEND_FILE_MAX_AGE_DEFAULT']
 clicked_date = ""
 
+
 @app.route('/', methods=['GET'])
 def main():
     return render_template("calendar.html")
@@ -13,12 +14,19 @@ def main():
 def date():
     clicked_date = request.args['res']
     cardsFound = Event.get_access_granted_in_date(clicked_date)
+    cardsFound.sort(key = sortBy)
+    for card in cardsFound:
+        hours = str(card.eventTime).split(' ')
+        card.eventTime = hours[1]
     print(clicked_date)
     return render_template("day.html", date=clicked_date, cardsCount = Event.get_count_in_date(clicked_date), cards = cardsFound)
 
 @app.route('/real_time', methods=['GET', 'POST'])
 def real_time():
     return render_template("real_time.html", event_date = "03.33.33")
+
+def sortBy(card):
+    return card.eventTime
 
 if __name__=='__main__':
     app.run(debug=True)
