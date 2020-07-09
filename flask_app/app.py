a224e052afb0cf34e7d8d0ba1df406e417ab581b
Blagovest Atanasov <blagovest.t.atanasov.2016@elsys-bg.org>
2020-07-09 13:37:20 +0300

    adding home page; functional navbars(desktop and mobile)

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,13 +2,14 @@ from flask import Flask, jsonify, request, render_template, redirect, url_for
 from ..database_connection.event import Event
 
 app = Flask(__name__)
+app.debug = True
 # app.config['SEND_FILE_MAX_AGE_DEFAULT']
 clicked_date = ""
 
 
 @app.route('/', methods=['GET'])
 def main():
-    return render_template("calendar.html")
+    return render_template("home.html")
 
 @app.route('/date', methods=['GET'])
 def date():
@@ -24,6 +25,10 @@ def date():
 @app.route('/real_time', methods=['GET', 'POST'])
 def real_time():
     return render_template("real_time.html", event_date = "03.33.33")
+
+@app.route('/calendar', methods=['GET', 'POST'])
+def calendar():
+    return render_template("calendar.html")
 
 def sortBy(card):
     return card.eventTime
